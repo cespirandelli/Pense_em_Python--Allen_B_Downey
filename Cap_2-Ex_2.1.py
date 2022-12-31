0103dfca9509d9d02a168e9409970111adf8598f
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2022-12-30 23:39:10 -0300

    Create Cap_2-Ex_2.1.py
    Exercício 2.1 - Capítulo 2

---
+++ Cap_2-Ex_2.1.py
@@ -0,0 +1,5 @@
+#### print(42 = n) # SyntaxError: cannot assign to literal
+#### print(x=y=1) # SyntaxError: invalid syntax
+print("Hello");
+#### print(1). # SyntaxError: invalid syntax
+#### print(xy) # NameError: name 'xy' is not defined
