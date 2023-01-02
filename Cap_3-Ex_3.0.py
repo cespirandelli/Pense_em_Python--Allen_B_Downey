fada2be80901b1f48328fb52ff1b51c88ced0c7d
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2023-01-01 23:19:55 -0300

    Create Cap_3-Ex_3.0.py
    Exercício 3.0 - Capítulo 3

---
+++ Cap_3-Ex_3.0.py
@@ -0,0 +1,15 @@
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+
+# Nem sempre se deve ler um programa de cima para baixo. Às vezes, é mais útil
+# começar no meio e ler para cima e para baixo. Por exemplo, para encontrar o
+# erro em um programa, você pode querer começar no ponto onde o programa
+# falha e procurar para trás. Às vezes, o ponto onde o programa falha é
+# antes do ponto onde você começou a procurar.
