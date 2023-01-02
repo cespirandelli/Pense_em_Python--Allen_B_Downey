100af5df8e08fad0aaabe1bc13819e3411eff2ce
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2023-01-01 23:29:57 -0300

    Update Cap_3-Ex_3.0.py

--- Cap_3-Ex_3.0.py
+++ Cap_3-Ex_3.0.py
@@ -13,3 +13,23 @@ repeat_lyrics()
 # erro em um programa, você pode querer começar no ponto onde o programa
 # falha e procurar para trás. Às vezes, o ponto onde o programa falha é
 # antes do ponto onde você começou a procurar.
+
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+# Quando você cria uma variável dentro de uma função, ela é local, ou seja, ela só
+# existe dentro da função. Por exemplo:
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+
+# Esta função recebe dois argumentos, concatena-os e exibe o resultado duas vezes.
+# Se você tentar usar cat fora da função, você recebe um erro: "cat" não existe,
+# porque cat é local a cat_twice.
+# print(cat) #NameError: name 'cat' is not defined
+
+line1 = 'Bing tiddle '
+line2 = 'tiddle bang.'
+cat_twice(line1, line2)
