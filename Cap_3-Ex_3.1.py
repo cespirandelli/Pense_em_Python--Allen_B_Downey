503010bd9bf32cece2ca42c171f71918e9d1dbe0
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2023-01-01 23:46:25 -0300

    Create Cap_3-Ex_3.1.py
    Exercício 3.1 - Capítulo 3

---
+++ Cap_3-Ex_3.1.py
@@ -0,0 +1,8 @@
+# Escreva um função chamada right_justify, que receba uma string chamada s como parâmetro e 
+# exiba a string com espaços suficientes à frente para que a última letra da string esteja 
+# na coluna 70 da tela:
+
+def right_justify(monty):
+    print("                                                                       ", monty)
+
+right_justify('César')
