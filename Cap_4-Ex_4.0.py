885ad36870045849eed4a92aee10c39cc237e28a
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2023-01-03 22:14:35 -0300

    Create Cap_4-Ex_4.0.py
    Exercício 4.0 - Capítulo 4 - Estudo de caso

---
+++ Cap_4-Ex_4.0.py
@@ -0,0 +1,23 @@
+# Estudo de caso: projeto interface
+'''
+---- Um plado de desenvolvimento
+
+Um plano de desenvolvimento é um processo para escrever programas. O processo
+que usamos neste estudo de caso é "encapsulamento e generalização". Os passos
+são:
+
+1. Comece escrevendo um pequeno programa sem definições de função.
+
+2. Uma vez que o programa esteja funcionando, identifique uma parte coerente
+dele, encapsule a parte em uma função e dê um nome a essa função.
+
+3. Geralize a função acrescentando parâmetros adequados.
+
+4. Repita os passos 1 a 3 até que você tenha uma coleção de funções operantes.
+Copie e cole esse código operante para evitar redigitação (e redepuração).
+
+5. Procure oportunidades de melhorar o programa peça refatoração. Por exemplo,
+se você tem um código semelhante em vários lugares, pode ser uma boa ideia 
+fatorá-lo em uma função geral adequada.
+'''
+
