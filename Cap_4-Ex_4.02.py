23323ac098a45676f2c88054a04a3ee244831dd5
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2023-01-03 22:05:13 -0300

    Update Cap_4-Ex_4.02.py

--- Cap_4-Ex_4.02.py
+++ Cap_4-Ex_4.02.py
@@ -18,16 +18,13 @@ def square(t):
         t.fd(200)
         t.rt(90)
 
-#desenhar_quadrado()
-#square(bob)
-turtle.mainloop
+#turtle.mainloop
 
 
 # 2) Adicione outro parâmetro, chamado length, à função square. Altere o corpo para que o
 #    comprimento dos lados do quadrado seja length, e então modifique a chamada de função
 #    para fornecer um segundo argumento. Execute o programa novamente. Teste sua função com
 #    um valor diferente para length.
-
 def square(t, lenght):
     for direitas in range(4):
         t.fd(lenght)
@@ -38,30 +35,64 @@ def square(t, lenght):
 # 3) Faça uma cópia da função square e mude o nome para polygon. Adicione outro parâmetro
 #    chamado n e altere o corpo para que desenhe um polígono de n lados. Dica: o ângulo
 #    interno de um polígono regular de n lados é 360/n graus.
-
-def polygon(t, lenght, n):
+def polygon(turtle, lenght, n):
     for direitas in range(n):
-        t.fd(lenght*n)
-        t.rt(360/n)
-#polygon(bob, 3, 12)
+        turtle.fd(lenght)
+        turtle.rt(360.0/n)
+
+#polygon(turtle=bob, lenght=70, n=7)
 
 
 # 4) Escreva uma função chamada circle que receba um turtle, t, e um raio, r, como parâmetros
 #    e que desenhe um círculo aproximado ao chamar polygon com um comprimento e número de lados
 #    adequados. Teste sua função com una série de valores de r.
 #    Dica: descubra a circunferência de um círculo e certifique-se de que length * n = circumference.
+def circle(t,r):
+    circumference = 2 * math.pi * r
+    n = int(circumference/3) + 1
+    length = circumference / n
+    polygon(t, length, n)
 
-def circle(turtle, r):
-    polygon(turtle, (2 * math.pi * r), 10)
-#circle(bob, 2)
+#circle(bob, 35)
 
 
 # 5) Faça uma versão mais geral do circle chamada arc, que que receba um parâmetro adicional de
 #    angle, para determinar qual fração do círculo deve ser desenhada. Angle está em unidades de graus,
 #    então quando angle=360, arc deve desenhar um círculo completo.
+def arco(t, r, angle):
+    tamanho_arco = 2 * math.pi * r * angle / 360
+    n = int(tamanho_arco/3) + 1
+    tamanho_passo = tamanho_arco / n
+    angulo_curva = angle/n
 
-def arc(turtle, r, angle):
-    turtle.pu()
-    polygon(turtle, (2 * math.pi * r), 10)
+    for movimentacao in range(n):
+        t.fd(tamanho_passo)
+        t.rt(angulo_curva)
 
-arc(bob, 1, 180)
+#arco(bob, 80, 296)
+
+
+####################################################################
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.rt(angle)
+
+def polygon(t, n, length):
+    angle = 360.0 / n
+    polyline(t, n, length, angle)
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length/3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle)/n
+    polyline(t,n, step_length, step_angle)
+
+def circle(t,r):
+    arc(t, r, 360)
+####################################################################
+
+
+arc(bob, 50, 90)
+circle(bob, 100)
